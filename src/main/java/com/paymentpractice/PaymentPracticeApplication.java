5d7d2d0715f2ba3bb50dd34b76b5c17e82aeb82b
rim109 <fla7722@naver.com>
2024-11-26 20:59:07 +0900

    feat:  초기설정

---
+++ src/main/java/com/paymentpractice/PaymentPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.paymentpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paymentpractice/PaymentPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
